a6bcea9c11128d0fbc34a00685b2dd7c63a2375f
soloproyectos <gonzalo.chumillas@gmail.com>
2017-09-06 21:12:48 +0200

    update dist

--- dist/easyvg.d.ts
+++ dist/easyvg.d.ts
@@ -1,23 +1,44 @@
 import { Transformable } from 'matrix';
-import { Point, Transformation } from 'matrix2';
+import { Point, Vector, Transformation } from 'matrix2';
 export declare class SvgElement<Type extends SVGElement> {
     readonly nativeElement: Type;
     constructor(target: string | Type, attributes?: {
         [key: string]: any;
     });
+    readonly ownerElement: SvgGraphicElement;
     getAttr(name: string): string;
     setAttr(name: string, value: any): SvgElement<Type>;
+    removeAttr(name: string): SvgElement<Type>;
     append(element: SvgElement<SVGElement>): void;
 }
 export declare class SvgGraphicElement extends SvgElement<SVGGraphicsElement> implements Transformable {
-    readonly transformation: Transformation;
-    transform(t: Transformation): Transformable;
-}
-export declare class SvgPath extends SvgGraphicElement {
-    private _strokeColor;
-    private _strokeWidth;
-    constructor();
-    moveTo(value: Point): SvgPath;
-    lineTo(value: Point): SvgPath;
-    close(): SvgPath;
+    private _isDraggingInit;
+    private _isDragging;
+    constructor(target: string | SVGGraphicsElement, attributes?: {
+        [key: string]: any;
+    });
+    private _initDragging();
+    onStartDragging(listener: (init: Point) => void): SvgGraphicElement;
+    onDragging(listener: (p: Point) => void): SvgGraphicElement;
+    onStopDragging(listener: (p: Point) => void): SvgGraphicElement;
+    transformation: Transformation;
+    transform(t: Transformation): SvgGraphicElement;
+    translate(value: Vector): SvgGraphicElement;
+    rotate(angle: number, params?: {
+        center: boolean;
+    }): SvgGraphicElement;
+    scale(value: number | Vector, params?: {
+        center: boolean;
+    }): SvgGraphicElement;
+    skew(value: number | Vector, params?: {
+        center: boolean;
+    }): SvgGraphicElement;
+    getBoundingBox(): {
+        x: number;
+        y: number;
+        width: number;
+        height: number;
+    };
+    private _getCenter();
+    private _getClientTransformation();
 }
--- dist/easyvg.js
+++ dist/easyvg.js
@@ -24,11 +24,22 @@ define(["require", "exports", "matrix2"], function (require, exports, matrix2_1)
                 this.setAttr(key, attributes[key]);
             }
         }
+        Object.defineProperty(SvgElement.prototype, "ownerElement", {
+            get: function () {
+                return new SvgGraphicElement(this.nativeElement.ownerSVGElement);
+            },
+            enumerable: true,
+            configurable: true
+        });
         SvgElement.prototype.getAttr = function (name) {
             return this.nativeElement.getAttributeNS(null, name);
         };
         SvgElement.prototype.setAttr = function (name, value) {
             this.nativeElement.setAttributeNS(null, name, '' + value);
+            return this;
+        };
+        SvgElement.prototype.removeAttr = function (name) {
+            this.nativeElement.removeAttributeNS(null, name);
             return this;
         };
         SvgElement.prototype.append = function (element) {
@@ -39,9 +50,49 @@ define(["require", "exports", "matrix2"], function (require, exports, matrix2_1)
     exports.SvgElement = SvgElement;
     var SvgGraphicElement = (function (_super) {
         __extends(SvgGraphicElement, _super);
-        function SvgGraphicElement() {
-            return _super !== null && _super.apply(this, arguments) || this;
+        function SvgGraphicElement(target, attributes) {
+            if (attributes === void 0) { attributes = {}; }
+            var _this = _super.call(this, target, attributes) || this;
+            _this._isDraggingInit = false;
+            _this._isDragging = false;
+            return _this;
         }
+        SvgGraphicElement.prototype.onStartDragging = function (listener) {
+            var self = this;
+            if (!this._isDraggingInit) {
+                this._initDragging();
+            }
+            this.nativeElement.addEventListener('mousedown', function (event) {
+                var t = self._getClientTransformation();
+                var p = new matrix2_1.Vector(event.clientX, event.clientY).transform(t);
+                listener.apply(self, [p]);
+            });
+            return this;
+        };
+        SvgGraphicElement.prototype.onDragging = function (listener) {
+            var self = this;
+            if (!this._isDraggingInit) {
+                this._initDragging();
+            }
+            document.addEventListener('mousemove', function (event) {
+                if (self._isDragging) {
+                    var t = self._getClientTransformation();
+                    var p = new matrix2_1.Vector(event.clientX, event.clientY).transform(t);
+                    listener.apply(self, [p]);
+                }
+            });
+            return this;
+        };
+        SvgGraphicElement.prototype.onStopDragging = function (listener) {
+            var self = this;
+            if (!this._isDraggingInit) {
+                this._initDragging();
+            }
+            this.nativeElement.addEventListener(self._stopDraggingEventName, function (event) {
+                listener.apply(self, [event.detail]);
+            });
+            return this;
+        };
         Object.defineProperty(SvgGraphicElement.prototype, "transformation", {
             get: function () {
                 var style = window.getComputedStyle(this.nativeElement, null);
@@ -56,6 +107,14 @@ define(["require", "exports", "matrix2"], function (require, exports, matrix2_1)
                 }
                 return ret;
             },
+            set: function (value) {
+                if (value === undefined || value === null) {
+                    this.removeAttr('transform');
+                }
+                else {
+                    this.setAttr('transform', value.toString());
+                }
+            },
             enumerable: true,
             configurable: true
         });
@@ -63,34 +122,78 @@ define(["require", "exports", "matrix2"], function (require, exports, matrix2_1)
             this.setAttr('transform', this.transformation.transform(t).toString());
             return this;
         };
+        SvgGraphicElement.prototype.translate = function (value) {
+            this.transform(new matrix2_1.Transformation().translate(value));
+            return this;
+        };
+        SvgGraphicElement.prototype.rotate = function (angle, params) {
+            var center = params !== undefined && params.center
+                ? this._getCenter()
+                : new matrix2_1.Vector(0, 0);
+            return this.transform(new matrix2_1.Transformation().rotate(angle, { center: center }));
+        };
+        SvgGraphicElement.prototype.scale = function (value, params) {
+            var center = params !== undefined && params.center
+                ? this._getCenter()
+                : new matrix2_1.Vector(0, 0);
+            return this.transform(new matrix2_1.Transformation().scale(value, { center: center }));
+        };
+        SvgGraphicElement.prototype.skew = function (value, params) {
+            var center = params !== undefined && params.center
+                ? this._getCenter()
+                : new matrix2_1.Vector(0, 0);
+            return this.transform(new matrix2_1.Transformation().skew(value, { center: center }));
+        };
+        SvgGraphicElement.prototype.getBoundingBox = function () {
+            var box = this.nativeElement.getBBox();
+            return { x: box.x, y: box.y, width: box.width, height: box.height };
+        };
+        SvgGraphicElement.prototype._generateId = function () {
+            var t = function (repeat) {
+                if (repeat === void 0) { repeat = 1; }
+                var ret = [];
+                for (var i = 0; i < repeat; i++) {
+                    ret.push(Math
+                        .floor((1 + Math.random()) * 0x10000)
+                        .toString(16)
+                        .substring(1));
+                }
+                return ret.join('');
+            };
+            return t(2) + '_' + t() + '_' + t() + '_' + t() + '_' + t(3);
+        };
+        SvgGraphicElement.prototype._initDragging = function () {
+            var self = this;
+            this._stopDraggingEventName = "stopdragging_" + this._generateId();
+            this.nativeElement.addEventListener('mousedown', function (event) {
+                self._isDragging = true;
+            });
+            for (var _i = 0, _a = ['mouseup', 'mouseleave', 'blur']; _i < _a.length; _i++) {
+                var eventName = _a[_i];
+                document.addEventListener(eventName, function (event) {
+                    if (self._isDragging) {
+                        var t = self._getClientTransformation();
+                        var p = event instanceof MouseEvent
+                            ? new matrix2_1.Vector(event.clientX, event.clientY).transform(t)
+                            : null;
+                        self.nativeElement.dispatchEvent(new CustomEvent(self._stopDraggingEventName, { detail: p }));
+                    }
+                    self._isDragging = false;
+                });
+            }
+            this._isDraggingInit = true;
+        };
+        SvgGraphicElement.prototype._getCenter = function () {
+            var box = this.getBoundingBox();
+            var center = new matrix2_1.Vector(box.x + box.width / 2, box.y + box.height / 2);
+            return center.transform(this.transformation);
+        };
+        SvgGraphicElement.prototype._getClientTransformation = function () {
+            var canvas = this.ownerElement;
+            var ctm = canvas.nativeElement.getScreenCTM();
+            return matrix2_1.Transformation.createFromValues(ctm.a, ctm.b, ctm.c, ctm.d, ctm.e, ctm.f).inverse();
+        };
         return SvgGraphicElement;
     }(SvgElement));
     exports.SvgGraphicElement = SvgGraphicElement;
-    var SvgPath = (function (_super) {
-        __extends(SvgPath, _super);
-        function SvgPath() {
-            var _this = _super.call(this, 'path') || this;
-            _this._strokeColor = 'black';
-            _this._strokeWidth = 2;
-            _this
-                .setAttr('stroke', _this._strokeColor)
-                .setAttr('stroke-width', _this._strokeWidth)
-                .setAttr('fill', 'transparent');
-            return _this;
-        }
-        SvgPath.prototype.moveTo = function (value) {
-            this.setAttr('d', [this.getAttr('d') || '', "M" + value.x + " " + value.y].join(' '));
-            return this;
-        };
-        SvgPath.prototype.lineTo = function (value) {
-            this.setAttr('d', [this.getAttr('d') || '', "L" + value.x + " " + value.y].join(' '));
-            return this;
-        };
-        SvgPath.prototype.close = function () {
-            this.setAttr('d', [this.getAttr('d') || '', 'Z'].join(' '));
-            return this;
-        };
-        return SvgPath;
-    }(SvgGraphicElement));
-    exports.SvgPath = SvgPath;
 });
