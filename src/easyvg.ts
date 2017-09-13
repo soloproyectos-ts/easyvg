5dc134e5b3d5c93962981bded5834f5e130feadb
soloproyectos <gonzalo.chumillas@gmail.com>
2017-09-14 00:23:43 +0200

    replace tabs by spaces

--- src/easyvg.ts
+++ src/easyvg.ts
@@ -4,11 +4,11 @@ import {Transformable} from 'matrix';
 import {Point, Vector, Transformation} from 'matrix2';
 
 export class SvgElement<Type extends SVGElement> {
-	readonly nativeElement: Type;
+  readonly nativeElement: Type;
 
   constructor (
-		target: string | Type, attributes: {[key: string]: any} = {}
-	) {
+    target: string | Type, attributes: {[key: string]: any} = {}
+  ) {
     if ( typeof target == 'string' ) {
       this.nativeElement = <Type> document.createElementNS(
         'http://www.w3.org/2000/svg', target
@@ -22,9 +22,9 @@ export class SvgElement<Type extends SVGElement> {
     }
   }
 
-	get ownerElement(): SvgGraphicElement {
-		return new SvgGraphicElement(this.nativeElement.ownerSVGElement);
-	}
+  get ownerElement(): SvgGraphicElement {
+    return new SvgGraphicElement(this.nativeElement.ownerSVGElement);
+  }
 
   getAttr(name: string): string {
     return this.nativeElement.getAttributeNS(null, name);
@@ -36,11 +36,11 @@ export class SvgElement<Type extends SVGElement> {
     return this;
   }
 
-	removeAttr(name: string): SvgElement<Type> {
-		this.nativeElement.removeAttributeNS(null, name);
+  removeAttr(name: string): SvgElement<Type> {
+    this.nativeElement.removeAttributeNS(null, name);
 
-		return this;
-	}
+    return this;
+  }
 
   append(element: SvgElement<SVGElement>): void {
     this.nativeElement.appendChild(element.nativeElement);
@@ -48,210 +48,210 @@ export class SvgElement<Type extends SVGElement> {
 }
 
 export class SvgGraphicElement
-	extends SvgElement<SVGGraphicsElement>
-	implements Transformable {
-	private _stopDraggingEventName: string;
-	private _isDraggingInit: boolean = false;
-	private _isDragging: boolean = false;
+  extends SvgElement<SVGGraphicsElement>
+  implements Transformable {
+  private _stopDraggingEventName: string;
+  private _isDraggingInit: boolean = false;
+  private _isDragging: boolean = false;
 
-	constructor(
-		target: string | SVGGraphicsElement, attributes: {[key: string]: any} = {}
-	) {
-		super(target, attributes);
-	}
+  constructor(
+    target: string | SVGGraphicsElement, attributes: {[key: string]: any} = {}
+  ) {
+    super(target, attributes);
+  }
 
-	onStartDragging(listener: (init: Point) => void): SvgGraphicElement {
+  onStartDragging(listener: (init: Point) => void): SvgGraphicElement {
     let self = this;
 
-		if (!this._isDraggingInit) {
-			this._initDragging();
-		}
+    if (!this._isDraggingInit) {
+      this._initDragging();
+    }
 
     this.nativeElement.addEventListener('mousedown', function (event) {
-			let t = self._getClientTransformation();
-			let p = new Vector(event.clientX, event.clientY).transform(t);
+      let t = self._getClientTransformation();
+      let p = new Vector(event.clientX, event.clientY).transform(t);
 
       listener.apply(self, [p]);
     });
 
-		return this;
+    return this;
   }
 
   onDragging(listener: (p: Point) => void): SvgGraphicElement {
     let self = this;
 
-		if (!this._isDraggingInit) {
-			this._initDragging();
-		}
+    if (!this._isDraggingInit) {
+      this._initDragging();
+    }
 
     document.addEventListener('mousemove', function (event) {
       if (self._isDragging) {
-				let t = self._getClientTransformation();
+        let t = self._getClientTransformation();
         let p = new Vector(event.clientX, event.clientY).transform(t);
 
         listener.apply(self, [p]);
       }
     });
 
-		return this;
+    return this;
   }
 
-	onStopDragging(listener: (p: Point) => void): SvgGraphicElement {
-		let self = this;
+  onStopDragging(listener: (p: Point) => void): SvgGraphicElement {
+    let self = this;
 
-		if (!this._isDraggingInit) {
-			this._initDragging();
-		}
+    if (!this._isDraggingInit) {
+      this._initDragging();
+    }
 
-		this.nativeElement.addEventListener(
-				self._stopDraggingEventName,
-				function (event: CustomEvent) {
-					listener.apply(self, [event.detail]);
-				}
-		);
+    this.nativeElement.addEventListener(
+        self._stopDraggingEventName,
+        function (event: CustomEvent) {
+          listener.apply(self, [event.detail]);
+        }
+    );
 
-		return this;
-	}
+    return this;
+  }
 
   get transformation(): Transformation {
-		let style = window.getComputedStyle(this.nativeElement, null);
-		let value = style.getPropertyValue('transform');
-		let matches = value.match(
-			/^matrix\(([^,]+),([^,]+),([^,]+),([^,]+),([^,]+),([^,]+)\)$/
-		);
-		let ret = new Transformation();
+    let style = window.getComputedStyle(this.nativeElement, null);
+    let value = style.getPropertyValue('transform');
+    let matches = value.match(
+      /^matrix\(([^,]+),([^,]+),([^,]+),([^,]+),([^,]+),([^,]+)\)$/
+    );
+    let ret = new Transformation();
 
-		if (matches !== null) {
-			let [a, b, c, d, e, f] = matches
-				.filter((elem, index) => index > 0)
-				.map((match) => parseFloat(match));
+    if (matches !== null) {
+      let [a, b, c, d, e, f] = matches
+        .filter((elem, index) => index > 0)
+        .map((match) => parseFloat(match));
 
-			ret = Transformation.createFromValues(a, b, c, d, e, f);
-		}
+      ret = Transformation.createFromValues(a, b, c, d, e, f);
+    }
 
-		return ret;
+    return ret;
   }
 
-	set transformation(value: Transformation) {
-		if (value === undefined || value === null) {
-			this.removeAttr('transform');
-		} else {
-			this.setAttr('transform', value.toString());
-		}
-	}
+  set transformation(value: Transformation) {
+    if (value === undefined || value === null) {
+      this.removeAttr('transform');
+    } else {
+      this.setAttr('transform', value.toString());
+    }
+  }
 
-	transform(t:Transformation): SvgGraphicElement {
-		this.setAttr('transform', this.transformation.transform(t).toString());
+  transform(t:Transformation): SvgGraphicElement {
+    this.setAttr('transform', this.transformation.transform(t).toString());
 
-		return this;
-	}
+    return this;
+  }
 
-	translate(value: Vector): SvgGraphicElement {
-		this.transform(new Transformation().translate(value));
+  translate(value: Vector): SvgGraphicElement {
+    this.transform(new Transformation().translate(value));
 
-		return this;
-	}
+    return this;
+  }
 
-	rotate(angle: number, params?: {center: boolean}): SvgGraphicElement {
-		let center = params !== undefined && params.center
-			? this._getCenter()
-			: new Vector(0, 0);
+  rotate(angle: number, params?: {center: boolean}): SvgGraphicElement {
+    let center = params !== undefined && params.center
+      ? this._getCenter()
+      : new Vector(0, 0);
 
-		return this.transform(
-			new Transformation().rotate(angle, {center: center})
-		);
-	}
+    return this.transform(
+      new Transformation().rotate(angle, {center: center})
+    );
+  }
 
-	scale(value: number|Vector, params?: {center: boolean}): SvgGraphicElement {
-		let center = params !== undefined && params.center
-			? this._getCenter()
-			: new Vector(0, 0);
+  scale(value: number|Vector, params?: {center: boolean}): SvgGraphicElement {
+    let center = params !== undefined && params.center
+      ? this._getCenter()
+      : new Vector(0, 0);
 
-		return this.transform(
-			new Transformation().scale(value, {center: center})
-		);
-	}
+    return this.transform(
+      new Transformation().scale(value, {center: center})
+    );
+  }
 
-	skew(value: number|Vector, params?: {center: boolean}): SvgGraphicElement {
-		let center = params !== undefined && params.center
-			? this._getCenter()
-			: new Vector(0, 0);
+  skew(value: number|Vector, params?: {center: boolean}): SvgGraphicElement {
+    let center = params !== undefined && params.center
+      ? this._getCenter()
+      : new Vector(0, 0);
 
-		return this.transform(
-			new Transformation().skew(value, {center: center})
-		);
-	}
+    return this.transform(
+      new Transformation().skew(value, {center: center})
+    );
+  }
 
-	getBoundingBox(): {x: number, y: number, width: number, height: number} {
-		let box = this.nativeElement.getBBox();
+  getBoundingBox(): {x: number, y: number, width: number, height: number} {
+    let box = this.nativeElement.getBBox();
 
     return {x: box.x, y: box.y, width: box.width, height: box.height};
-	}
+  }
 
-	// Generates a random ID.
-	//
-	// Thanks to: https://stackoverflow.com/a/105074/1704895
-	private _generateId(): string {
-	  let t = (repeat: number = 1) => {
-			let ret = [];
+  // Generates a random ID.
+  //
+  // Thanks to: https://stackoverflow.com/a/105074/1704895
+  private _generateId(): string {
+    let t = (repeat: number = 1) => {
+      let ret = [];
 
-			for (let i = 0; i < repeat; i++) {
-				ret.push(Math
-					.floor((1 + Math.random()) * 0x10000)
-		      .toString(16)
-		      .substring(1));
-			}
+      for (let i = 0; i < repeat; i++) {
+        ret.push(Math
+          .floor((1 + Math.random()) * 0x10000)
+          .toString(16)
+          .substring(1));
+      }
 
-			return ret.join('');
-	  }
+      return ret.join('');
+    }
 
-	  return t(2) + '_' + t() + '_' + t() + '_' + t() + '_' + t(3);
-	}
+    return t(2) + '_' + t() + '_' + t() + '_' + t() + '_' + t(3);
+  }
 
-	// Initializes the dragging
-	private _initDragging() {
-		let self = this;
+  // Initializes the dragging
+  private _initDragging() {
+    let self = this;
 
-		this._stopDraggingEventName = `stopdragging_${this._generateId()}`;
+    this._stopDraggingEventName = `stopdragging_${this._generateId()}`;
 
     this.nativeElement.addEventListener('mousedown', function (event) {
-			self._isDragging = true;
+      self._isDragging = true;
     });
 
     for (let eventName of ['mouseup', 'mouseleave', 'blur']) {
       document.addEventListener(eventName, function (event) {
-				if (self._isDragging) {
-					let t = self._getClientTransformation();
-					let p = event instanceof MouseEvent
-						? new Vector(event.clientX, event.clientY).transform(t)
-						: null;
+        if (self._isDragging) {
+          let t = self._getClientTransformation();
+          let p = event instanceof MouseEvent
+            ? new Vector(event.clientX, event.clientY).transform(t)
+            : null;
 
-					self.nativeElement.dispatchEvent(
-						new CustomEvent(self._stopDraggingEventName, {detail: p})
-					);
-				}
+          self.nativeElement.dispatchEvent(
+            new CustomEvent(self._stopDraggingEventName, {detail: p})
+          );
+        }
 
-				self._isDragging = false;
+        self._isDragging = false;
       });
     }
 
-		this._isDraggingInit = true;
-	}
+    this._isDraggingInit = true;
+  }
 
-	// Gets the center from the parent's reference system.
-	private _getCenter(): Point {
-		let box = this.getBoundingBox();
-		let center = new Vector(box.x + box.width / 2, box.y + box.height / 2);
+  // Gets the center from the parent's reference system.
+  private _getCenter(): Point {
+    let box = this.getBoundingBox();
+    let center = new Vector(box.x + box.width / 2, box.y + box.height / 2);
 
-		return center.transform(this.transformation);
-	}
+    return center.transform(this.transformation);
+  }
 
-	private _getClientTransformation(): Transformation {
-		let canvas = this.ownerElement;
-		let ctm = canvas.nativeElement.getScreenCTM();
+  private _getClientTransformation(): Transformation {
+    let canvas = this.ownerElement;
+    let ctm = canvas.nativeElement.getScreenCTM();
 
     return Transformation.createFromValues(
       ctm.a, ctm.b, ctm.c, ctm.d, ctm.e, ctm.f
     ).inverse();
-	}
+  }
 }
